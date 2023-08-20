9cc094ca269adbd5af68381bc77e2084a08fbb3e
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-08-20 02:05:04 -0400

    Add data visualization using chart js

--- Frontend/src/RepoSearch.jsx
+++ Frontend/src/RepoSearch.jsx
@@ -1,6 +1,11 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 import axios from "axios";
+// import { ArcElement, Legend, Tooltip } from "chart.js";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const RepoSearch = () => {
   // * track the input value and the result
@@ -22,21 +27,21 @@ const RepoSearch = () => {
     const links = {};
 
     //split parts by comma (extract individual links)
-    const parts = header.split(',');
+    const parts = header.split(",");
 
-    // For each link, extract the URL and the relation type 
+    // For each link, extract the URL and the relation type
     //(e.g. next, prev, last, first)
     for (let i = 0; i < parts.length; i++) {
-      const section = parts[i].split(';');
+      const section = parts[i].split(";");
       if (section.length !== 2) {
         throw new Error("section could not be split on ';'");
       }
-      const url = section[0].replace(/<(.*)>/, '$1').trim();
-      const name = section[1].replace(/rel="(.*)"/, '$1').trim();
+      const url = section[0].replace(/<(.*)>/, "$1").trim();
+      const name = section[1].replace(/rel="(.*)"/, "$1").trim();
       links[name] = url;
     }
     return links;
-  }
+  };
 
   //fetch all pages of closed PRS
   const fetchAllClosedPRs = async (url) => {
@@ -44,19 +49,19 @@ const RepoSearch = () => {
       setLoading(true);
       const response = await axios.get(url, {
         headers: {
-          Authorization: `token ${token}`
-        }
+          Authorization: `token ${token}`,
+        },
       });
-  
+
       // Parse link header to get the pagination links
       const links = parseLinkHeader(response.headers.link);
-      
+
       // If there's a next page, fetch it recursively
       if (links && links.next) {
         const nextPageData = await fetchAllClosedPRs(links.next);
         return [...response.data, ...nextPageData];
       }
-  
+
       // If no next page, return the current page data
       return response.data;
     } catch (error) {
@@ -66,13 +71,13 @@ const RepoSearch = () => {
     }
   };
 
-//    * Calculate the time to merge for each closed pull requests
-//       * and then calculate the average 
+  //    * Calculate the time to merge for each closed pull requests
+  //       * and then calculate the average
   const fetchTimeToMerge = async () => {
     try {
       setLoading(true);
       //clear previous result when loading starts
-      setAvgTimeToMerge(null); 
+      setAvgTimeToMerge(null);
       // Extract owner and repo from the repoLink
       const matches = repoLink.match(/github\.com\/([^\/]+)\/([^\/]+)/);
       if (!matches) {
@@ -95,11 +100,12 @@ const RepoSearch = () => {
         const createdAt = new Date(pr.created_at);
         const mergedAt = new Date(pr.merged_at);
         // Convert milliseconds to hours
-        return (mergedAt - createdAt) / (1000 * 60 * 60); 
+        return (mergedAt - createdAt) / (1000 * 60 * 60);
       });
       //total time taken to merge all PRs
       const total = timesToMerge.reduce((acc, curr) => acc + curr, 0);
-      const average = total / timesToMerge.length ? total / timesToMerge.length : 0;
+      const average =
+        total / timesToMerge.length ? total / timesToMerge.length : 0;
       //update the state with the computed avg time to merge
       setAvgTimeToMerge(average);
       setLoading(false);
@@ -111,9 +117,46 @@ const RepoSearch = () => {
 
   // * when user submits the form, call the fetchTimeToMerge function
   const handleSubmit = (event) => {
-    event.preventDefault(); 
+    event.preventDefault();
     fetchTimeToMerge();
   };
+
+// Helper function to calculate the datasets for the Doughnut chart based on avgTimeToMerge
+const getChartData = (avgTime) => {
+  if (avgTime <= 24) {
+    return {
+      labels: ["Within 24 Hours", "Within 7 Days", "Beyond 7 Days"],
+      datasets: [
+        {
+          data: [24, 144 - 24, 744 - 144],
+          backgroundColor: ["#66ff66", "#FFFF00", "#FF6347"],
+        },
+      ],
+    };
+  } else if (avgTime > 24 && avgTime <= 168) {
+    return {
+      labels: ["Within 24 Hours", "Within 7 Days", "Beyond 7 Days"],
+      datasets: [
+        {
+          data: [0, 168, 744 - 168],
+          backgroundColor: ["#66ff66", "#FFFF00", "#FF6347"],
+        },
+      ],
+    };
+  } else {
+    return {
+      labels: ["Within 24 Hours", "Within 7 Days", "Beyond 7 Days"],
+      datasets: [
+        {
+          //31 days limit
+          data: [0, 0, 744],  
+          backgroundColor: ["#66ff66", "#FFFF00", "#FF6347"],
+        },
+      ],
+    };
+  }
+};
+
 
   return (
     <div>
@@ -126,12 +169,27 @@ const RepoSearch = () => {
         />
         <button type="submit">Fetch Data</button>
       </form>
-      {loading && ( 
-        <div>Loading...</div>
-      )}
+      {loading && <div>Loading...</div>}
       {avgTimeToMerge !== null && (
-        //format avgTimeToMerge to have exactly 2 decimal places
-        <div className="average-time">Average Time to Merge: {avgTimeToMerge.toFixed(2)} hours</div>
+        <div>
+          <Doughnut
+            data={getChartData(avgTimeToMerge)}
+            options={{
+              circumference: 180,
+              rotation: 270,
+              responsive: true,
+              legend: {
+                display: false,
+              },
+              tooltips: {
+                enabled: false,
+              },
+            }}
+          />
+          <div className="average-time">
+            Average Time to Merge: {avgTimeToMerge.toFixed(2)} hours
+          </div>
+        </div>
       )}
     </div>
   );
